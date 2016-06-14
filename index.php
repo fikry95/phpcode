6bc5bc411ba14c7710950ea61fe163d79ac24240
fikry95 <husni.alfikri.dalimunthe@students.uin-suska.ac.id>
2016-06-15 00:39:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-    echo "this is the world";
+    class index{
+        public function __construct(){
+            include 'define.php';
+        }
+        public function viewAll(){
+         $sql ="SELECT*FROM $this->table ORDER BY id";
+         $stmt=$this->prepare($sql);
+         $stmt->execute();
+            while($obj=$stmt->fetch(PDO::FETCH_LAZY)){
+                $data[]=$obj;
+            }
+        }
+        public function insert_data($id, $nama){
+            $sql="INSERT INTO $this->table (id, nama) value ($this->id, $this->nama)";
+            $stmt=$this->koneksi->prepare($sql);
+            $stmt->execute();
+        }
+    }
 ?>
